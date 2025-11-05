dee09d4d70c32866d96c34aa844024ab47657124
Indraneel Patil <indraneelrajeevpatil@gmail.com>
2025-11-05 08:13:07 -0800

    Finished record loop

--- neel/scripts/lerobot_record.py
+++ neel/scripts/lerobot_record.py
@@ -7,22 +7,29 @@ Record dataset from Lerobot
 from dataclasses import dataclass, field
 from pathlib import Path
 import time
+from typing import Any
+import logging
 
 from lerobot.configs import parser
-from lerobot.robots import RobotConfig, make_robot_from_config
-from lerobot.teleoperators import TeleoperatorConfig, make_teleoperator_from_config
+from lerobot.robots import RobotConfig, make_robot_from_config, Robot
+from lerobot.teleoperators import TeleoperatorConfig, make_teleoperator_from_config, Teleoperator
 from lerobot.configs.policies import PreTrainedConfig
+from lerobot.policies.pretrained import PreTrainedPolicy
 from lerobot.datasets.lerobot_dataset import LeRobotDataset
-from lerobot.datasets.utils import combine_feature_dicts
+from lerobot.datasets.utils import combine_feature_dicts, build_dataset_frame
 from lerobot.datasets.pipeline_features import aggregate_pipeline_dataset_features, create_initial_features
 from lerobot.utils.utils import init_logging
 from lerobot.utils.visualization_utils import init_rerun, log_rerun_data
-from lerobot.processor import make_default_processors
+from lerobot.processor import make_default_processors, RobotProcessorPipeline, RobotAction, RobotObservation, PolicyProcessorPipeline, PolicyAction
 from lerobot.utils.control_utils import (
     sanity_check_dataset_robot_compatibility,
     init_keyboard_listener
 )
 from lerobot.datasets.video_utils import VideoEncodingManager
+from lerobot.datasets.image_writer import safe_stop_image_writer
+from lerobot.utils.constants import ACTION, OBS_STR
+from lerobot.utils.robot_utils import busy_wait
+
 
 
 @dataclass
@@ -81,9 +88,79 @@ class RecordConfig:
     def __get_path_fields__(cls) -> list[str]:
         return ["policy"]
 
+@safe_stop_image_writer
+def record_loop(
+    robot: Robot,
+    events: dict,
+    fps: int,
+    teleop_action_processor: RobotProcessorPipeline[tuple[RobotAction, RobotObservation], RobotAction],
+    robot_action_processor: RobotProcessorPipeline[tuple[RobotAction,RobotObservation],RobotAction],
+    robot_observation_processor: RobotProcessorPipeline[RobotObservation, RobotObservation],
+    dataset: LeRobotDataset | None = None,
+    teleop: Teleoperator | list[Teleoperator] | None = None,
+    policy: PreTrainedPolicy | None = None,
+    preprocessor: PolicyProcessorPipeline[dict[str, Any], dict[str, Any]] | None = None,
+    postprocessor: PolicyProcessorPipeline[ PolicyAction, PolicyAction] | None = None,
+    control_time_s: int | None=None,
+    single_task: str | None = None,
+    display_data:bool = False
+):
+    if dataset is not None and dataset.fps != fps:
+        raise ValueError(f"The dataset fps should be equal to requested fps ({dataset.fps} != {fps})")
+    
+    teleop_arm = teleop_keyboard = None
+    
+    # TODO: Not handled multi teleop
 
-def record_loop():
-    pass
+    # TODO Policy stuff
+    
+    timestamp = 0
+    start_episode_t = time.perf_counter()
+    while timestamp < control_time_s:
+        start_loop_t = time.perf_counter()
+
+        if events["exit_early"]:
+            events["exit_early"] = False
+            break
+
+        # Get robot observation
+        obs = robot.get_observation()
+
+        # Applies a pipeline to the raw robot observation, default is IdentityProcessor
+        obs_processed = robot_observation_processor(obs)
+
+        if policy is not None or dataset is not None:
+            observation_frame = build_dataset_frame(dataset.features, obs_processed, prefix=OBS_STR)
+        
+        if policy is None and isinstance(teleop, Teleoperator):
+            act = teleop.get_action()
+
+            act_processed_teleop = teleop_action_processor((act, obs))
+        else:
+            logging.info("No policy or teleoperator provided, skipping action generation")
+            continue
+
+        action_values = act_processed_teleop
+        robot_action_to_send = robot_action_processor((act_processed_teleop, obs))
+
+        _sent_action = robot.send_action(robot_action_to_send)
+
+        # Write to dataset
+        if dataset is not None:
+            action_frame = build_dataset_frame(dataset.features, action_values, prefix=ACTION)
+            frame = {**observation_frame, **action_frame, "task": single_task}
+            dataset.add_frame(frame)
+        
+        if display_data:
+            log_rerun_data(observation=obs_processed, action= action_values)
+        
+        dt_s = time.perf_counter() - start_loop_t
+        busy_wait(1 / fps - dt_s)
+
+        timestamp = time.perf_counter() - start_episode_t
+
+
+
 
 @parser.wrap()
 def record(cfg: RecordConfig) -> LeRobotDataset:
@@ -139,7 +216,22 @@ def record(cfg: RecordConfig) -> LeRobotDataset:
         recorded_episodes = 0
         while recorded_episodes < cfg.dataset.num_episodes and not events["stop_recording"]:
             print(f"Recording episode {dataset.num_episodes}")
-            record_loop()
+            record_loop(
+                robot=robot,
+                events=events,
+                fps=cfg.dataset.fps,
+                teleop_action_processor=teleop_action_processor,
+                robot_action_processor=robot_action_processor,
+                robot_observation_processor=robot_observation_processor,
+                teleop=teleop,
+                policy=None,
+                preprocessor=None,
+                postprocessor=None,
+                dataset=dataset,
+                control_time_s=cfg.dataset.episode_time_s,
+                single_task=cfg.dataset.single_task,
+                display_data=cfg.display_data
+            )
 
             # Sleep for 10 seconds
             time.sleep(10)
